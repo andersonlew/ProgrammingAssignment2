e4d7eb82e4faa3334129f2f77f1207f0f9343e25
Lewis Anderson <anderson.lew@gmail.com>
2015-06-18 21:20:16 +0900

    Save cachematrix File

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,91 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Matrix inversion is usually a costly computation 
+## and there may be some benefit to caching the inverse 
+## of a matrix rather than compute it repeatedly. This 
+## function creates a special "matrix" object that can 
+## cache its inverse.
 
-## Write a short comment describing this function
+#########################################
+## Some examples of matrix multiplication
+#########################################
+
+# c <- rbind(c(1, -1/4), c(-1/4, 1))
+# solve(c)
+# 
+# library(MASS)
+# ginv(c)
+# 
+# c <- rbind(c(1, -1/4, 2), c(2, 1, -1/4), c(-1/4, 2, 1))
+# solve(c)
+# ginv(c)
+# 
+# c <- rbind(c(1, 2, 3), c(4, 1.5, -1/4), c(2.1, 2, 1))
+# solve(c)
+# ginv(c)
+# 
+# nrow(c)
+# ncol(c)
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinv <- function(solve) m <<- solve
+  getinv <- function() m
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## This function computes the inverse of the special 
+## "matrix" returned by makeCacheMatrix above. If the 
+## inverse has already been calculated (and the matrix 
+## has not changed), then the cachesolve should retrieve 
+## the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  m <- x$getinv()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinv(m)
+  m
 }
+
+
+########################################
+## This is an example of cache the mean
+#######################################
+
+# makeVector <- function(x = numeric()) {
+#   m <- NULL
+#   set <- function(y) {
+#     x <<- y
+#     m <<- NULL
+#   }
+#   get <- function() x
+#   setmean <- function(mean) m <<- mean
+#   getmean <- function() m
+#   list(set = set, get = get,
+#        setmean = setmean,
+#        getmean = getmean)
+# }
+# 
+# cachemean <- function(x, ...) {
+#   m <- x$getmean()
+#   if(!is.null(m)) {
+#     message("getting cached data")
+#     return(m)
+#   }
+#   data <- x$get()
+#   m <- mean(data, ...)
+#   x$setmean(m)
+#   m
+# }
